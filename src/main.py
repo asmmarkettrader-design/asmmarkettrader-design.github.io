31bf930620b336f42e2f95de2f0a496aac961af5
asmmarkettrader-design <asmmarkettrader@gmail.com>
2026-08-02 11:54:33 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -524,14 +524,14 @@ def get_html_header(title, categories_list=[], seo_desc="ASM VEO - Premium Onlin
 <!-- Meta Pixel Code -->
     <script>
     !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    {{if(f.fbq)return;n=f.fbq=function(){{n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)}};
     if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
     n.queue=[];t=b.createElement(e);t.async=!0;
     t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    s.parentNode.insertBefore(t,s)}}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '123456789012345'); /* یہاں بعد میں آپ اپنی اصلی پکسل آئی ڈی لگا سکتے ہیں */
+    fbq('init', '123456789012345'); 
     fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
